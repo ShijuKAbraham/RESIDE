b67024aab94c3f218dc9bc03f727db8fde68fa9e
ShijuKAbraham <k.ashiju10@gmail.com>
2019-07-18 22:37:44 +0530

    register and login page added

--- reside/settings.py
+++ reside/settings.py
@@ -35,7 +35,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
-    'realtors.apps.RealtorsConfig',   
+    'realtors.apps.RealtorsConfig', 
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
